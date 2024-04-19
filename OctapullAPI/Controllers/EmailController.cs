e3e9fff564dbdf5fe7ade4eb715a8558b8c40cc3
MSoykan <mehmetsoykanmutlu@gmail.com>
2024-04-19 23:45:57 +0300

    EmailService almost done
    - Added mailkit to business layer
    - Added EmailService interface and respective service
    - Email sending functionality is working with ethereal. A mock smtp website. Using test account in order to send emails. Test account credentials can be found in appsettings.json
    - Sending email to user upon succesful registration
    - TO DO : Sending mails for meetings

---
+++ OctapullAPI/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.EmailService.Abstract;
+using BusinessLayer.EmailService.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OctapullAPI.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase {
+        private readonly IEmailService emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request) {
+            emailService.SendEmail(request);
+
+            return Ok();
+        }
+
+    }
+}
